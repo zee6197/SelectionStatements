974ed66ac66e9c54c2433fb2eb76be7f9d62fa56
zee6197 <zeeman@berkeley.edu>
2022-04-26 12:50:48 -0700

    Commit

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            Console.WriteLine("Please enter a number: ");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if ( userInput < favNumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("too high");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+        }
+    }
+}
